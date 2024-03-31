ba126cd9d68cc205391eb829d3c432b0e53aa3fb
dev-bala <balachandran@cloudi5.com>
2024-03-31 17:14:00 +0530

    jwt checking

--- src/utils/Jwt.js
+++ src/utils/Jwt.js
@@ -2,5 +2,4 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => jwt.sign({id: user.id}, process.env.JWT_SECRET_KEY, { expiresIn : '60m' });
 
-return generateToken;
 module.exports = generateToken
